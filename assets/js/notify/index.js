487ed41f6accd800839c81aaec980a9ffb79580e
abdohantash <abdohantash9999@gmail.com>
2022-07-25 16:51:14 +0200

    first commit

---
+++ assets/js/notify/index.js
@@ -0,0 +1,12 @@
+'use strict';
+var notify = {
+    type: 'theme',
+    allow_dismiss: true,
+    delay: 2000,
+    showProgressbar: true,
+    timer: 300,
+    animate:{
+        enter:'animated fadeInDown',
+        exit:'animated fadeOutUp'
+    }
+};
