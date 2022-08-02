e02ae3dea782ed8977a10ea5d2e15d8db90e3d04
abdohantash <abdohantash9999@gmail.com>
2022-08-02 16:50:17 +0200

    style

--- assets/js/script.js
+++ assets/js/script.js
@@ -489,6 +489,46 @@ $(".hijri-date-input").hijriDatePicker({
     });
   });
 
+
+(function($) {
+
+  // Init empty gallery array
+  var container = [];
+
+  // Loop over gallery items and push it to the array
+  $('.imgs-auc').find('li').each(function() {
+    var $link = $(this).find('a'),
+      item = {
+        src: $link.attr('href'),
+        w: $link.data('width'),
+        h: $link.data('height'),
+        title: $link.data('caption')
+      };
+    container.push(item);
+  });
+
+  // Define click event on gallery item
+  $('.imgs-auc li a').click(function(event) {
+
+    // Prevent location change
+    event.preventDefault();
+
+    // Define object and gallery options
+    var $pswp = $('.pswp')[0],
+      options = {
+        index: $(this).parent('li').index(),
+        bgOpacity: 0.85,
+        showHideOpacity: true
+      };
+
+    // Initialize PhotoSwipe
+    var gallery = new PhotoSwipe($pswp, PhotoSwipeUI_Default, container, options);
+    gallery.init();
+  });
+
+}(jQuery));
+
+
 // $('.tagsinput').tagsInput();
 
 var myCountdown = new countdown({
